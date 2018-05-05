7a747532e2872018947b6d38148d17554f7c7826
mackenziepl <mackenziepl@poczta.onet.pl>
2018-05-05 22:24:21 +0200

    Zadanie: Poszerzenie testów

--- src/test/java/com/crud/tasks/scheduler/EmailSchedulerTest.java
+++ src/test/java/com/crud/tasks/scheduler/EmailSchedulerTest.java
@@ -1,6 +1,10 @@
-package com.crud.tasks.service;
+package com.crud.tasks.scheduler;
 
+import com.crud.tasks.config.AdminConfig;
 import com.crud.tasks.domain.Mail;
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -9,33 +13,41 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public class SimpleEmailServiceTest {
-    @InjectMocks
-    private SimpleEmailService simpleEmailService;
+public class EmailSchedulerTest {
 
     @Mock
     private JavaMailSender javaMailSender;
 
+    @InjectMocks
+    private SimpleEmailService simpleEmailService;
+
     @Test
-    public void shouldSendEmail() {
-        //Given
-        Mail mail = new Mail("test@test.com", "Test", "Test Message", "test2@test.com");
+    public void sendInformationEmail() {
+        long size = 2;
+        String link = " task";
+        if(size > 1) {
+            link = " tasks";
+        }
+
+        Mail mail = new Mail("mack.kodilla@gmail.com", "Tasks: Once a day email",
+                "Currently in database you got: " + size + link, null);
 
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
-        mailMessage.setCc(mail.getToCc());
 
-        //When
         simpleEmailService.send(mail);
 
-        //Then
         verify(javaMailSender, times(1)).send(mailMessage);
     }
 }
